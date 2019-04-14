d838163f654460e71b94cc83123aa141a9bb2384
Kirill Gorbikov <kirillhor@gmail.com>
2019-04-14 18:07:51 +0300

    feat: add new solutions

--- task/02-numbers-tasks.js
+++ task/02-numbers-tasks.js
@@ -22,7 +22,8 @@
  *   5, 5  => 25
  */
 function getRectangleArea(width, height) {
-    throw new Error('Not implemented');
+    return width*height;
+    /*throw new Error('Not implemented');*/
 }
 
 
@@ -38,7 +39,8 @@ function getRectangleArea(width, height) {
  *   0    => 0
  */
 function getCicleCircumference(radius) {
-    throw new Error('Not implemented');
+    return 2*Math.PI*radius
+    /*throw new Error('Not implemented');*/
 }
 
 /**
@@ -54,7 +56,8 @@ function getCicleCircumference(radius) {
  *  -3, 3  => 0
  */
 function getAverage(value1, value2) {
-    throw new Error('Not implemented');
+    return (value1/2 + value2/2);
+    /*throw new Error('Not implemented');*/
 }
 
 /**
@@ -73,7 +76,8 @@ function getAverage(value1, value2) {
  *   (-5,0) (10,-10) => 18.027756377319946
  */
 function getDistanceBetweenPoints(x1, y1, x2, y2) {
-    throw new Error('Not implemented');
+    return Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));
+    /*throw new Error('Not implemented');*/
 }
 
 /**
